3bc9059c967887ddaf79c52acb9654786a1c7ac9
GOA-homeworks <nikachochia2008@gmail.com>
2024-07-07 22:07:51 +0400

    commit

---
+++ level1/homework/hw.py
@@ -0,0 +1,13 @@
+firstname = "ნიკა" #ეს არის str (string) ტიპის ცვლადი
+ #firstname არის ცვლადი
+ # = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+ # "ნიკა" და ა.შ არის ცვლადისთვის მნიშვნელობა
+lastname = "ჩოჩია"
+age = "16"# ეს არის int (integer) მთელი რიცხვი
+heigh = "178" #ეს არის float ტიპის ცვლადი (ათწილადი)
+ #boolean (bool) ტიპის ცვლადი
+country = "საქართველო"
+favouritefood = "ხინკალი"
+favouritemovie = "აზზე არ ვარ"
+print(firstname, lastname, age, heigh, country, favouritefood, favouritemovie)
+ #print ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
